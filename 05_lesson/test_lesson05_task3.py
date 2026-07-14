99456a1d5c44bc1048132a4603d2004a7bae3eb0
fedorovatkach1964 <fedorovatkach@gmail.com>
2026-07-14 15:13:02 +0300

    Внесены изменения в файл test_lesson05_task3.py

--- 05_lesson/test_lesson05_task3.py
+++ 05_lesson/test_lesson05_task3.py
@@ -11,7 +11,7 @@ def test_multiple_elements():
     # Найдите все ссылки на странице (тег <a>).
     num_element = driver.find_elements(By.TAG_NAME, "a")
     for i, num_el in enumerate(num_element, 1):
-        print(f'Номер элемента {i} ')
+        assert num_el.is_displayed()
 
     # Проверьте, что количество ссылок равно 9.
     assert len(num_element) == 9
@@ -19,4 +19,5 @@ def test_multiple_elements():
     # Проверьте, что текст первой ссылки содержит "1".
     first_element = num_element[0].text
     assert first_element == "1"
+
     driver.quit()
